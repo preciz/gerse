c1bea8741c7782adfa12ab31cc26095f60e69275
preciz <barna.kovacs.codes@gmail.com>
2017-01-28 15:48:16 +0100

    Add all links to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,19 +9,31 @@ export default class Header extends Component {
           className="flex items-center"
           style="max-width: 1170px; margin: auto;"
         >
-          <div className="header__logo mr1"></div>
+          <div className="header__logo mr1" style={{minWidth: 30}} />
           <h1 className="color-white m0 h3 pr4 fw900">GERSEKARÁT</h1>
-          <nav className="flex">
+          <nav className="flex items-center">
             <Link
-              className="color-white text-decoration-none pr2 bold fw900"
+              className="color-white text-decoration-none pr2 bold fw900 center"
               href="/"
             >HIREK</Link>
             <Link
-              className="color-white text-decoration-none px2 bold fw900"
+              className="color-white text-decoration-none px2 bold fw900 center"
               href="/kozigazgatas"
             >KOZIGAZGATAS</Link>
             <Link
-              className="color-white text-decoration-none px2 bold fw900"
+              className="color-white text-decoration-none px2 bold fw900 center"
+              href="/kozigazgatas"
+            >INTEZEMENYEK</Link>
+            <Link
+              className="color-white text-decoration-none px2 bold fw900 center"
+              href="/kozigazgatas"
+            >TURIZMUS</Link>
+            <Link
+              className="color-white text-decoration-none px2 bold fw900 center"
+              href="/kozigazgatas"
+            >CIVIL SZERVEZETEK</Link>
+            <Link
+              className="color-white text-decoration-none px2 bold fw900 center"
               href="/gbk"
             >BARATI KOR</Link>
           </nav>
