72a9ba70f79c4013cf4b77bf8ae57961fae8ba00
preciz <barna.kovacs.codes@gmail.com>
2017-01-28 15:50:53 +0100

    Use new palyazatok data from window obj

--- src/components/gbk/palyazatok.js
+++ src/components/gbk/palyazatok.js
@@ -1,13 +1,12 @@
 import { h, Component } from 'preact';
 import Palyazat from './palyazat';
-import palyazatok_data from './palyazatok_data';
 
 export default class Palyazatok extends Component {
   render() {
     return (
       <div>
         {
-          palyazatok_data.map((palyazat) =>
+          window.gbk_palyazatok.map((palyazat) =>
             <Palyazat
               palyazat={palyazat}
             />
