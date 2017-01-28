edd31496b4b20e7f5f3c3a6e44fc3eb9a5101119
preciz <barna.kovacs.codes@gmail.com>
2017-01-28 15:27:06 +0100

    Update header style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,18 +9,19 @@ export default class Header extends Component {
           className="flex items-center"
           style="max-width: 1170px; margin: auto;"
         >
-          <h1 className="color-white m0 h3 pr4">GERSEKARAT</h1>
+          <div className="header__logo mr1"></div>
+          <h1 className="color-white m0 h3 pr4 fw900">GERSEKARÁT</h1>
           <nav className="flex">
             <Link
-              className="color-white text-decoration-none pr2 bold"
+              className="color-white text-decoration-none pr2 bold fw900"
               href="/"
             >HIREK</Link>
             <Link
-              className="color-white text-decoration-none px2 bold"
+              className="color-white text-decoration-none px2 bold fw900"
               href="/kozigazgatas"
             >KOZIGAZGATAS</Link>
             <Link
-              className="color-white text-decoration-none px2 bold"
+              className="color-white text-decoration-none px2 bold fw900"
               href="/gbk"
             >BARATI KOR</Link>
           </nav>
