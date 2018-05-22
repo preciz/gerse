3d1b26211fd49b61c90e176f299879fa23b7676b
preciz <barna.kovacs.codes@gmail.com>
2018-05-22 21:16:20 +0200

    Change email

--- src/components/header_mobile/index.js
+++ src/components/header_mobile/index.js
@@ -70,7 +70,7 @@ export default class HeaderMobile extends Component {
                     <path fill="#fff" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
               </svg>
             </a>
-            <a target="_blank" href="mailto:gersebkor@freemail.hu">
+            <a target="_blank" href="mailto:gerse01@gersekarat.hu">
               <svg style="width:28px;height:28px" viewBox="0 0 22 22">
                 <path fill="#fff" d="M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
               </svg>
